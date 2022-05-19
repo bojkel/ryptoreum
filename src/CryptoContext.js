27d6663317ef42a270f770fb0098bebba9b249f4
Izrudin <marblezstars@gmail.com>
2022-05-20 01:58:26 +0300

    ryptoreum!

---
+++ src/CryptoContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+  const [currency, setCurrency] = useState("DKK");
+  const [symbol, setSymbol] = useState("kr");
+
+  useEffect(() => {
+    if (currency === "DKK") setSymbol("kr. ");
+    else if(currency === "EUR") setSymbol("€")
+    else if (currency === "USD") setSymbol("$");
+  }, [currency]);
+
+  return (
+    <Crypto.Provider value={{ currency, setCurrency, symbol }}>
+      {children}
+    </Crypto.Provider>
+  );
+};
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+  return useContext(Crypto);
+};
